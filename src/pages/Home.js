1827e38c8522a81d1e2540accf85c618fc7af34d
christianngouw <christiangouw168@gmail.com>
2021-10-17 23:49:55 +0700

    feat: add services

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { fetchRandomIdeas } from '../service/ideas.js'
 
 const Home = () => {
+
+  function getRandomIdeas(){
+      fetchRandomIdeas()
+  }
+
   return (
     <div>
       <div className="content-wrapper">
         <div className="heading-text">Let's get some ideas.</div>
         <div>
-          <button className="btn-primary">Random</button>
+          <button onClick={getRandomIdeas} className="btn-primary">Random</button>
         </div>
       </div>
     </div>
---
+++ src/service/ideas.js
@@ -0,0 +1,5 @@
+import axios from '../utils/axios';
+
+export const fetchRandomIdeas = () => {
+  return axios.get()
+}
