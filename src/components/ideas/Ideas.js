9a32361b80d8030b4532f39e3077cf298d34e9d7
christianngouw <christiangouw168@gmail.com>
2021-10-21 00:54:59 +0700

    feat: implement new card design and save idea feature

--- src/components/ideas/Ideas.js
+++ src/components/ideas/Ideas.js
@@ -1,14 +1,48 @@
 import React from "react";
+import LocalPresistent from "../../service/localPresistent";
 
 const Ideas = ({ idea }) => {
+  const localPresistent = new LocalPresistent();
+
+  const save = (data) => {
+    localPresistent.save(data);
+  };
+
   return (
     <div className="card">
-      <div className="card-header">
-        <div className="card-title">"{idea.activity}"</div>
-        <button className="btn-primary">Save</button>
+      <div className="card-inner-left">
+        <div style={{ height: "100%" }}>
+          <div style={{ fontWeight: 600 }} className="card-title">
+            #{idea.key}
+          </div>
+          <div className="card-title" style={{ marginTop: "2rem" }}>
+            {idea.activity}
+          </div>
+        </div>
       </div>
-      <div className="card-body">
-        <div> Type: {idea.type} </div>
+      <div className="card-inner-right">
+        <div className="card-inner-container">
+          <div className="inner-right-header">
+            <div className="card-heading">Detail</div>
+          </div>
+          <div className="inner-right-container">
+            <div className="inner-right-content">
+              <div>
+                <div>Type: {idea.type}</div>
+                <div>Participants: {idea.participants}</div>
+              </div>
+              <div>
+                <div>Accessibility: {idea.accessibility}</div>
+                <div>Price: {idea.price}</div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <button className="btn-primary" onClick={() => save(idea)}>
+              save
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
