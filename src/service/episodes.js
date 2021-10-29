4ccfd9745dde2750078bc8da2d97be21a90f0d69
christianngouw <christiangouw168@gmail.com>
2021-10-29 23:42:22 +0700

    chore: add new service

--- src/service/episodes.js
+++ src/service/episodes.js
@@ -3,3 +3,7 @@ import axios from "../utils/axios";
 export const getEpisode = (episode) => {
   return axios.get(`/episodes/${episode}`);
 };
+
+export const getEpisodeName = (episode) => {
+  return axios.get(`/episode/${episode}`).then((res) => res.data.name);
+};
